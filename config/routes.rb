522d4c380336630517a69d72966253a37df24b5d
Luca G. Soave <luca.soave@gmail.com>
2013-07-04 19:42:35 +0200

    signin_path GET /auth/:provider/callback(.:format) gitkeeper/sessions#create

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Gitkeeper::Engine.routes.draw do
   # get '/signin', to: 'sessions#new'
   get '/signout', to: 'sessions#destroy'
    # get '/auth/:provider', to: 'errors#error'
-  get '/auth/:provider/callback', to: 'sessions#create'
+  get '/auth/:provider/callback', to: 'sessions#create', as: 'signin'
 end
