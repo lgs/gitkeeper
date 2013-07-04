3ac28542176b3837ba9419f8f3edfdee00ed8aa2
Luca G. Soave <luca.soave@gmail.com>
2013-07-04 18:12:00 +0200

    config/initializers/omniauth-github.rb & sessions routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,6 @@
 Gitkeeper::Engine.routes.draw do
+  # get '/signin' => 'sessions#new', to: :signin
+  get '/signout' => 'sessions#destroy', to: :signout
+  get '/auth/:provider/callback', to: 'sessions#create'
+  # get '/auth/:provider', to: 'errors#error'
 end
