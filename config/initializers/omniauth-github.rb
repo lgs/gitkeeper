9d57114e2b271ea75d3196de512bc8409dd9d931
Luca G. Soave <luca.soave@gmail.com>
2014-04-06 15:52:50 +0200

    edit README

--- config/initializers/omniauth-github.rb
+++ config/initializers/omniauth-github.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   #require 'openid/store/filesystem' 
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
   #provider :openid, :store => OpenID::Store::Filesystem.new('/tmp')
+
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
 end
 
